02975b85965b40514f9c11a8c4ef815d98327ce6
Paul <paul.lugagnedelpon@gmail.com>
2015-03-20 00:19:17 +0800

    feat(navbar): use $state for all the links

--- client/components/footer/footer.controller.js
+++ client/components/footer/footer.controller.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('arimorcApp')
-  .controller('FooterCtrl', function ($scope, $location) {
+  .controller('FooterCtrl', function ($scope, $location, $state) {
 
     $scope.isActive = function(route) {
       return (route === $location.path())? 'active' : '';
     };
+
+    $scope.goTo = function(uri) {
+      $state.go(uri);
+    }
 
   });
--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,21 +1,25 @@
 'use strict';
 
 angular.module('arimorcApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth, $translate) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
+  .controller('NavbarCtrl', function ($scope, $location, Auth, $translate, $state) {
+    // $scope.menu = [{
+    //   'title': 'Home',
+    //   'link': '/'
+    // }];
 
-    $scope.isCollapsed = true;
-    $scope.isLoggedIn = Auth.isLoggedIn;
-    $scope.isAdmin = Auth.isAdmin;
-    $scope.getCurrentUser = Auth.getCurrentUser;
+    // $scope.isCollapsed = true;
+    // $scope.isLoggedIn = Auth.isLoggedIn;
+    // $scope.isAdmin = Auth.isAdmin;
+    // $scope.getCurrentUser = Auth.getCurrentUser;
 
-    $scope.logout = function() {
-      Auth.logout();
-      $location.path('/login');
-    };
+    // $scope.logout = function() {
+    //   Auth.logout();
+    //   $location.path('/login');
+    // };
+
+    $scope.goTo = function(uri) {
+      $state.go(uri);
+    }
 
     $scope.isActive = function(route) {
       return (route === $location.path())? 'active' : '';
