8a6852d355e7959d76cf5ad52ac9f0389a62c2fb
Paul <paul.lugagnedelpon@gmail.com>
2015-03-27 00:24:41 +0800

    feat(ux): force scroll to the top when going to new page

--- client/app/app.js
+++ client/app/app.js
@@ -43,7 +43,7 @@ angular.module('arimorcApp', [
     };
   })
 
-  .run(function ($rootScope, $location, Auth) {
+  .run(function ($rootScope, $location, Auth, $anchorScroll) {
     // Redirect to login if route requires auth and you're not logged in
     $rootScope.$on('$stateChangeStart', function (event, next) {
       Auth.isLoggedInAsync(function(loggedIn) {
@@ -51,6 +51,11 @@ angular.module('arimorcApp', [
           $location.path('/login');
         }
       });
+    });
+
+    // Force scroll on top when new page is loaded
+    $rootScope.$on("$locationChangeSuccess", function() {
+      $anchorScroll();
     });
   })
 
